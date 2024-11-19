9ae81e8c05c45e3bd4c3a565afa7310b7e62e6d7
kedistkid <kedistkid723@gmail.com>
2024-11-19 12:02:23 +0300

    split gene ontology

--- biocypher_metta/adapters/gene_ontology_adapter.py
+++ biocypher_metta/adapters/gene_ontology_adapter.py
@@ -4,9 +4,39 @@ class GeneOntologyAdapter(OntologyAdapter):
     ONTOLOGIES = {
         'go': 'http://purl.obolibrary.org/obo/go.owl'
     }
+    
+    # GO subontology types
+    BIOLOGICAL_PROCESS = 'biological_process'
+    MOLECULAR_FUNCTION = 'molecular_function'
+    CELLULAR_COMPONENT = 'cellular_component'
+    
+    # Edge type mappings
+    EDGE_LABELS = {
+        'biological_process': 'biological_process_subclass_of',
+        'molecular_function': 'molecular_function_subclass_of',
+        'cellular_component': 'cellular_component_subclass_of'
+    }
 
-    def __init__(self, write_properties, add_provenance, ontology, type, label='go', dry_run=False, add_description=False, cache_dir=None):        
-        super(GeneOntologyAdapter, self).__init__(write_properties, add_provenance, ontology, type, label, dry_run, add_description, cache_dir)
+    def __init__(self, write_properties, add_provenance, ontology, type, label=None, 
+                 dry_run=False, add_description=False, cache_dir=None):
+        super(GeneOntologyAdapter, self).__init__(write_properties, add_provenance, 
+                                                 ontology, type, label, dry_run, 
+                                                 add_description, cache_dir)
+        
+        if type == 'node':
+            self.current_subontology = label if label in [
+                self.BIOLOGICAL_PROCESS,
+                self.MOLECULAR_FUNCTION,
+                self.CELLULAR_COMPONENT
+            ] else None
+        else:  # type == 'edge'
+            # Extract the base subontology from the edge label
+            for subonto, edge_label in self.EDGE_LABELS.items():
+                if label == edge_label:
+                    self.current_subontology = subonto
+                    break
+            else:
+                self.current_subontology = None
 
     def get_ontology_source(self):
         """
@@ -17,26 +47,31 @@ class GeneOntologyAdapter(OntologyAdapter):
     def find_go_nodes(self, graph):
         # subontologies are defined as `namespaces`
         nodes_in_namespaces = list(graph.subject_objects(predicate=OntologyAdapter.NAMESPACE))
-
         node_namespace_lookup = {}
         for n in nodes_in_namespaces:
             node = n[0]
             namespace = n[1]
-
             node_key = OntologyAdapter.to_key(node)
             node_namespace_lookup[node_key] = str(namespace)
         return node_namespace_lookup
 
     def get_nodes(self):
-        # Convert the generator to a list
         nodes = list(super().get_nodes())
-
-        # Find GO nodes and their namespaces
-        if self.graph is not None:
+        if self.graph is not None and self.current_subontology:
             nodes_in_go_namespaces = self.find_go_nodes(self.graph)
             for node_id, label, props in nodes:
-                props['subontology'] = nodes_in_go_namespaces.get(node_id, None)
-                yield node_id, label, props
-        else:
-            for node_id, label, props in nodes:
-                yield node_id, label, props
+                namespace = nodes_in_go_namespaces.get(node_id, None)
+                if namespace == self.current_subontology:
+                    yield node_id, label, props
+
+    def get_edges(self):
+        edges = list(super().get_edges())
+        if self.graph is not None and self.current_subontology:
+            nodes_in_go_namespaces = self.find_go_nodes(self.graph)
+            edge_label = self.EDGE_LABELS[self.current_subontology]
+            
+            for source, target, rel_type, props in edges:
+                source_namespace = nodes_in_go_namespaces.get(source, None)
+                # Only need to check source namespace since GO enforces subontology boundaries
+                if source_namespace == self.current_subontology:
+                    yield source, target, edge_label, props
