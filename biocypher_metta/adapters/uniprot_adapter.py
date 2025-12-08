e2d24c734d14b65ba30182481a34c63a2431dbdf
sauloapp <sauloaugusto@gmail.com>
2025-12-08 13:28:36 -0300

    Changes in uniprot_adapter to handle muultiple species data.

--- biocypher_metta/adapters/uniprot_adapter.py
+++ biocypher_metta/adapters/uniprot_adapter.py
@@ -1,28 +1,35 @@
 import gzip
-import re
-from Bio import SeqIO
+from Bio import SeqIO, SwissProt
 from biocypher_metta.adapters import Adapter
 
 # Data file is uniprot_sprot_human.dat.gz and uniprot_trembl_human.dat.gz at https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/.
 # We can use SeqIO from Bio to read the file.
 # Each record in file will have those attributes: https://biopython.org/docs/1.75/api/Bio.SeqRecord.html
-# id, name will be loaded for protein. Ensembl IDs(example: ENST00000372839.7) in dbxrefs will be used to create protein and transcript relationship.
+# id, name will be loaded for protein. Ensembl IDs(example: Ensembl:ENST00000372839.7) in dbxrefs will be used to create protein and transcript relationship.
+
+
+# Check whether the data is compatible to the organisms (species). Add new species entries here...
+translation_condition_map = {
+    7227: lambda item, entry_name: bool(item[0].startswith('EnsemblMetazoa') and 'FBtr' in item[1] and  entry_name.endswith("DROME")),
+    9606: lambda item, entry_name: bool(item[0].startswith('Ensembl') and 'ENST' in item[1] and entry_name.endswith("HUMAN")),
+}
+
 
 
 class UniprotAdapter(Adapter):
+    
     ALLOWED_TYPES = ['translates to', 'translation of']
     ALLOWED_LABELS = ['translates_to', 'translation_of']
-    
-    ISOFORM_PATTERN = re.compile(r'\[([\w\-]+)\]')
 
-    def __init__(self, filepath, type, label, taxon_id, write_properties=True, add_provenance=True):
-        if type not in self.ALLOWED_TYPES:
-            raise ValueError('Invalid type. Allowed values: ' + 
-                           ', '.join(self.ALLOWED_TYPES))
-        if label not in self.ALLOWED_LABELS:
-            raise ValueError('Invalid label. Allowed values: ' + 
-                           ', '.join(self.ALLOWED_LABELS))
-            
+    # added "taxon_id" to the 'protein' schema
+    def __init__(self, filepath, type, label,
+                 write_properties, add_provenance, taxon_id):
+        if type not in UniprotAdapter.ALLOWED_TYPES:
+            raise ValueError('Invalid type. Allowed values: ' +
+                             ', '.join(UniprotAdapter.ALLOWED_TYPES))
+        if label not in UniprotAdapter.ALLOWED_LABELS:
+            raise ValueError('Invalid label. Allowed values: ' +
+                             ', '.join(UniprotAdapter.ALLOWED_LABELS))
         self.filepath = filepath
         self.dataset = label
         self.type = type
@@ -30,63 +37,131 @@ class UniprotAdapter(Adapter):
         self.source = "Uniprot"
         self.source_url = "https://www.uniprot.org/"
         self.taxon_id = taxon_id
-        
+
         super(UniprotAdapter, self).__init__(write_properties, add_provenance)
 
-    def parse_ensembl_reference(self, line):
-        try:
-            isoform_match = self.ISOFORM_PATTERN.search(line)
-            isoform_id = isoform_match.group(1) if isoform_match else None
-
-            parts = line.split(';')
-            if len(parts) >= 2:
-                transcript = parts[1].strip().split('.')[0]  
-                if transcript.startswith('ENST'):
-                    return transcript, isoform_id
-            return None, None
-        except Exception as e:
-            print(f"Error parsing line: {line}")
-            print(f"Error details: {str(e)}")
-            return None, None
 
     def get_edges(self):
-        current_protein_id = None
-        accession = None
-        
+        translation_conditions_hold = translation_condition_map[self.taxon_id]
+
+        # with gzip.open(self.filepath, 'rt') as input_file:
+        #     records = SeqIO.parse(input_file, 'swiss')
         with gzip.open(self.filepath, 'rt') as input_file:
-            for line in input_file:
-                line = line.strip()
+            records = SwissProt.parse(input_file)
+            for record in records:
+                if self.type == 'translates to':
+                    # dbxrefs = record.dbxrefs
+                    dbxrefs = record.cross_references
+                    for item in dbxrefs:
+                        # if item.startswith('Ensembl') and 'ENST' in item:                    
+                        if translation_conditions_hold(item, record.entry_name):
+                            try:
+                                ensg_id = "ENSEMBL:" + item[1].split(':')[-1].split('.')[0]  # Added ENSEMBL prefix
+                                uniprot_id = "UniProtKB:" + record.accessions[0].upper()  #record.id.upper()  # Added UniProtKB prefix
+                                _source = ensg_id
+                                _target = uniprot_id
+                                _props = {}
+                                _props['taxon_id'] = f'NCBITaxon:{self.taxon_id}'
+                                if self.write_properties and self.add_provenance:
+                                    _props['source'] = self.source
+                                    _props['source_url'] = self.source_url
+                                yield _source, _target, self.label, _props
+
+                            except :
+                                print(
+                                    f'fail to process for edge translates to: {record.entry_name} / {item[0]} / {item[1]}')
+                                pass
+
+
+# import gzip
+# import re
+# from Bio import SeqIO
+# from biocypher_metta.adapters import Adapter
+
+# # Data file is uniprot_sprot_human.dat.gz and uniprot_trembl_human.dat.gz at https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/.
+# # We can use SeqIO from Bio to read the file.
+# # Each record in file will have those attributes: https://biopython.org/docs/1.75/api/Bio.SeqRecord.html
+# # id, name will be loaded for protein. Ensembl IDs(example: ENST00000372839.7) in dbxrefs will be used to create protein and transcript relationship.
+
+
+# class UniprotAdapter(Adapter):
+#     ALLOWED_TYPES = ['translates to', 'translation of']
+#     ALLOWED_LABELS = ['translates_to', 'translation_of']
+    
+#     ISOFORM_PATTERN = re.compile(r'\[([\w\-]+)\]')
+
+#     def __init__(self, filepath, type, label, taxon_id, write_properties=True, add_provenance=True):
+#         if type not in self.ALLOWED_TYPES:
+#             raise ValueError('Invalid type. Allowed values: ' + 
+#                            ', '.join(self.ALLOWED_TYPES))
+#         if label not in self.ALLOWED_LABELS:
+#             raise ValueError('Invalid label. Allowed values: ' + 
+#                            ', '.join(self.ALLOWED_LABELS))
+            
+#         self.filepath = filepath
+#         self.dataset = label
+#         self.type = type
+#         self.label = label
+#         self.source = "Uniprot"
+#         self.source_url = "https://www.uniprot.org/"
+#         self.taxon_id = taxon_id
+        
+#         super(UniprotAdapter, self).__init__(write_properties, add_provenance)
+
+#     def parse_ensembl_reference(self, line):
+#         try:
+#             isoform_match = self.ISOFORM_PATTERN.search(line)
+#             isoform_id = isoform_match.group(1) if isoform_match else None
+
+#             parts = line.split(';')
+#             if len(parts) >= 2:
+#                 transcript = parts[1].strip().split('.')[0]  
+#                 if transcript.startswith('ENST'):
+#                     return transcript, isoform_id
+#             return None, None
+#         except Exception as e:
+#             print(f"Error parsing line: {line}")
+#             print(f"Error details: {str(e)}")
+#             return None, None
+
+#     def get_edges(self):
+#         current_protein_id = None
+#         accession = None
+        
+#         with gzip.open(self.filepath, 'rt') as input_file:
+#             for line in input_file:
+#                 line = line.strip()
                 
-                if line.startswith('ID '):
-                    current_protein_id = line.split()[1]
-                    accession = None
+#                 if line.startswith('ID '):
+#                     current_protein_id = line.split()[1]
+#                     accession = None
                     
-                elif line.startswith('AC '):
-                    accessions = line[5:].split()
-                    if accessions and accessions[0]:
-                        accession = accessions[0].rstrip(';')
+#                 elif line.startswith('AC '):
+#                     accessions = line[5:].split()
+#                     if accessions and accessions[0]:
+#                         accession = accessions[0].rstrip(';')
                         
-                elif line.startswith('DR   Ensembl;') and accession:
-                    transcript_id, isoform_id = self.parse_ensembl_reference(line)
+#                 elif line.startswith('DR   Ensembl;') and accession:
+#                     transcript_id, isoform_id = self.parse_ensembl_reference(line)
                     
-                    if transcript_id:
-                        try:
-                            protein_id = isoform_id if isoform_id else accession
+#                     if transcript_id:
+#                         try:
+#                             protein_id = isoform_id if isoform_id else accession
                             
-                            ensg_id = "" + transcript_id
-                            uniprot_id = "UniProtKB:" + protein_id.upper()
+#                             ensg_id = "" + transcript_id
+#                             uniprot_id = "UniProtKB:" + protein_id.upper()
                             
-                            _props = {}
-                            if self.write_properties and self.add_provenance:
-                                _props['source'] = self.source
-                                _props['source_url'] = self.source_url
+#                             _props = {}
+#                             if self.write_properties and self.add_provenance:
+#                                 _props['source'] = self.source
+#                                 _props['source_url'] = self.source_url
                             
-                            if self.type == 'translates to':
-                                yield ensg_id, uniprot_id, self.label, _props
-                            elif self.type == 'translation of':
-                                yield uniprot_id, ensg_id, self.label, _props
+#                             if self.type == 'translates to':
+#                                 yield ensg_id, uniprot_id, self.label, _props
+#                             elif self.type == 'translation of':
+#                                 yield uniprot_id, ensg_id, self.label, _props
                                 
-                        except Exception as e:
-                            print(f'Failed to process edge {self.type}: {current_protein_id}')
-                            print(f'Error: {str(e)}')
-                            continue
+#                         except Exception as e:
+#                             print(f'Failed to process edge {self.type}: {current_protein_id}')
+#                             print(f'Error: {str(e)}')
+#                             continue
