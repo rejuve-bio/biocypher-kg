715e1c898a3f45d38a5e79fcef08811204052646
sauloapp <sauloaugusto@gmail.com>
2026-01-12 09:02:43 -0300

    print messages on empty dicts entries for Roadmap adapters

--- biocypher_metta/adapters/hsa/roadmap_h3_marks_adapter.py
+++ biocypher_metta/adapters/hsa/roadmap_h3_marks_adapter.py
@@ -61,10 +61,10 @@ class RoadMapH3MarkAdapter(Adapter):
                         chr = self.dbsnp_rsid_map.get(_id, {}).get("chr", None) 
                         pos = self.dbsnp_rsid_map.get(_id, {}).get("pos", None)
                         if chr == None:
-                            print(f"chr is None for {_id}. Skipping it {_id}...")
+                            print(f"roadmap_h3_marks: chr is None for {_id}. Skipping it {_id}...")
                             continue
                         if pos == None:
-                            print(f"pos is None for {_id}. Skipping it {_id}...")
+                            print(f"roadmap_h3_marks: pos is None for {_id}. Skipping it {_id}...")
                             continue
                         cell_id = row[self.COL_DICT['cell']].split()[0]
                         biological_context = self.cell_to_ontology_id_map.get(cell_id, [None])[-1]
