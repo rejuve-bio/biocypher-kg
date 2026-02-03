c23a1882309a03493fe36047be0c69697788cc1d
Natnael Mesfin <108123522+natnaellT@users.noreply.github.com>
2026-02-03 10:59:11 +0300

    chebi adapter ename conflict resolution

--- biocypher_metta/adapters/chebi_ontology_adapter.py
+++ biocypher_metta/adapters/chebi_ontology_adapter.py
@@ -5,7 +5,7 @@ class ChEBIAdapter(OntologyAdapter):
         'chebi': 'https://purl.obolibrary.org/obo/chebi.owl'
     }
 
-    def __init__(self, write_properties, add_provenance, ontology, type, label='chemical_substance', dry_run=False, add_description=False, cache_dir=None):
+    def __init__(self, write_properties, add_provenance, ontology, type, label='small_molecule', dry_run=False, add_description=False, cache_dir=None):
         super().__init__(write_properties, add_provenance, ontology, type, label, dry_run, add_description, cache_dir)
 
     def get_ontology_source(self):
